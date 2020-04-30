513a515a1704589e5688db390c401a6eccaa1fcd
Aditi Desai <aditidesai795@gmail.com>
2020-04-30 11:46:37 -0400

    Search in a Binary Search Tree

---
+++ Easy/Search in a Binary Search Tree.java
@@ -0,0 +1,46 @@
+/*
+700. Search in a Binary Search Tree
+
+For example, 
+Given the tree:
+        4
+       / \
+      2   7
+     / \
+    1   3
+
+And the value to search: 2
+You should return this subtree:
+
+      2     
+     / \   
+    1   3
+*/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode searchBST(TreeNode root, int val) {
+        if(root == null){
+            return null;
+        }
+        if(root.val==val){
+            return root;
+        }
+        return (val < root.val?searchBST(root.left,val):                                 searchBST(root.right,val));
+    }
+}
+//O(logn)
